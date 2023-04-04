7102e34ffcb2308e2c202a7336e5a4e30a44b08a
LautaroLovato <lautaro.lovato07@gmail.com>
2023-04-04 13:42:58 -0300

    PlaygroundIntermedio2

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -3,12 +3,16 @@ from django.db import models
 class Curso(models.Model):
     nombre= models.CharField(max_length= 50) # campo de texto
     comision= models.IntegerField() # campo de numeros
+    def __str__(self):
+        return f"{self.nombre} - {self.comision}"
 
 class Estudiante(models.Model):
     nombre= models.CharField(max_length= 50)
     apellido= models.CharField(max_length= 40)
     email= models.EmailField() # campo de tipo email
-
+    def __str__(self):
+        return f"{self.nombre} - {self.apellido}"
+    
 class Profesor(models.Model):
     nombre= models.CharField(max_length= 50)
     apellido= models.CharField(max_length= 50)
